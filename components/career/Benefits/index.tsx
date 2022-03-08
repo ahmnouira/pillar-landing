da70ad81052fe596758a6e0c2fd6d478ab50edbf
Ahmed <ahmnouira@gmail.com>
2022-03-08 23:11:39 +0100

    add benefits section

--- components/career/Benefits/index.tsx
+++ components/career/Benefits/index.tsx
@@ -1,3 +1,4 @@
+import { Grid } from '@mui/material';
 import { handleMoveToId } from 'utils';
 import styles from './Benefits.module.scss';
 interface Props {
@@ -14,23 +15,29 @@ const benefits = [
 
 const Benefits: React.FC<Props> = ({ imgSrc }) => {
   return (
-    <section>
-      <img src="" />
-      <div className={styles['benefits-content']}>
-        <h6>CAREERS</h6>
-        <h1>Benefits and Perks</h1>
-        {benefits.map((benefit, idx) => (
-          <div key={idx}>
-            <img src="" />
-            <p className={`${styles['large']} large`}>{benefit}</p>
+    <section className={styles.benefits}>
+      <Grid container>
+        <Grid item xs={12} sm={6} sx={{}}>
+          <div className={styles['benefits-hero']} style={{ backgroundImage: `url(${imgSrc})` }} />
+        </Grid>
+        <Grid item xs={12} sm={6}>
+          <div className={styles['benefits-content']}>
+            <h6>CAREERS</h6>
+            <h1>Benefits and Perks</h1>
+            {benefits.map((benefit, idx) => (
+              <div key={idx} className={styles['benefits-content-item']}>
+                <img src="/career/plus.svg" alt="plus-icon" />
+                <p>{benefit}</p>
+              </div>
+            ))}
+            <div className={styles['button-container']}>
+              <button className="ui-button-2" onClick={() => handleMoveToId()}>
+                OPEN POSITIONS
+              </button>
+            </div>
           </div>
-        ))}
-        <div className={styles['button-container']}>
-          <button className="ui-button secondary" onClick={() => handleMoveToId()}>
-            OPEN POSITIONS
-          </button>
-        </div>
-      </div>
+        </Grid>
+      </Grid>
     </section>
   );
 };
