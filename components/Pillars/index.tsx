9a3907d3c5cb8b55644a28b951f9b72861d9264c
Ahmed <ahmnouira@gmail.com>
2022-03-14 19:34:09 +0100

    link to sponsor page

--- components/Pillars/index.tsx
+++ components/Pillars/index.tsx
@@ -1,4 +1,5 @@
 import { Grid } from '@mui/material';
+import Link from 'next/link';
 import PillarCard from './components/PillarCard';
 import styles from './Pillar.module.scss';
 
@@ -7,6 +8,7 @@ interface iPillarCards {
   subtitle: string;
   description: string;
   image: string;
+  path: string;
 }
 
 const pillarCardsContent: iPillarCards[] = [
@@ -16,6 +18,7 @@ const pillarCardsContent: iPillarCards[] = [
     subtitle: 'Investment & Operational Due Diligence, Deal Flow',
     description:
       "Detailed vetting of sponsors' people, processes, systems, and track records coupled with comprehensive asset-level data to facilitate efficient investment due diligence.",
+    path: '/#',
   },
   {
     image: '/landingpage/data-first.png',
@@ -23,6 +26,7 @@ const pillarCardsContent: iPillarCards[] = [
     subtitle: 'Standardized Reporting & Customizable Analytics',
     description:
       'Incorporating alternative data sources and up-to-date information from property management and accounting software to support a systematic investment process.',
+    path: '/#',
   },
   {
     image: '/landingpage/network-driven.png',
@@ -30,6 +34,7 @@ const pillarCardsContent: iPillarCards[] = [
     subtitle: 'Discovery, Diligence, & Monitoring',
     description:
       'Building confidence and facilitating transactions via a community of sophisticated investors: discuss new and ongoing deals, follow respected sponsors and investors.',
+    path: '/career',
   },
   {
     image: '/landingpage/capital-structure.png',
@@ -37,6 +42,7 @@ const pillarCardsContent: iPillarCards[] = [
     subtitle: 'From Acquisition to Recapitalization',
     description:
       'A marketplace to accommodate investor capital of varying time horizons, solving the liquidity needs of both investors and sponsors.',
+    path: '/sponsor',
   },
 ];
 
@@ -45,16 +51,18 @@ const Pillars = () => {
     <section className={styles.pillars}>
       <h3>Our Pillars</h3>
       <div className={styles['pillars-content']}>
-        {pillarCardsContent.map(({ title, subtitle, description, image }, index) => (
-          <Grid item xs={12} sm={6} lg={3} key={index}>
-            <PillarCard
-              title={title}
-              subtitle={subtitle}
-              description={description}
-              imageSrc={image}
-              styles={styles}
-            />
-          </Grid>
+        {pillarCardsContent.map(({ title, subtitle, description, image, path }, index) => (
+          <Link href={path} key={index}>
+            <Grid item xs={12} sm={6} lg={3}>
+              <PillarCard
+                title={title}
+                subtitle={subtitle}
+                description={description}
+                imageSrc={image}
+                styles={styles}
+              />
+            </Grid>
+          </Link>
         ))}
       </div>
     </section>
