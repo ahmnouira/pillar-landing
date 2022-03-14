051d35b802f79b8009fbceba4560f690d2f44486
Ahmed <ahmnouira@gmail.com>
2022-03-14 17:47:17 +0100

    fix text-field

--- components/CTA/components/TextField/index.tsx
+++ components/CTA/components/TextField/index.tsx
@@ -1,31 +1,42 @@
-import {
-  styled,
-  TextField as MuiTextField,
-  TextFieldProps,
-  textFieldClasses,
-  outlinedInputClasses,
-  inputClasses,
-  inputLabelClasses,
-} from '@mui/material';
+import { styled, TextField as MuiTextField, TextFieldProps } from '@mui/material';
 import React from 'react';
 
 const StyledTextField = styled(MuiTextField)`
-  height: 15px;
-  & .MuiInput-underline:hover {
+  height: 30px;
+  color: #fff;
+  font-size: 15px;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 15px;
+  font-family: 'IBM Plex Sans';
+  margin-bottom: 24px;
+
+  &:hover fieldset {
     border-color: #fff;
   }
 
-  & .MuiInput-underline:after {
+  &.MuiInput-root:hover:not(.Mui-disabled):before {
     border-color: #fff;
   }
 
-  & .MuiInput-underline:before {
+  & .MuiInput-underline {
     border-color: #fff;
+    &::after {
+      border-color: #fff;
+    }
+    &::before {
+      border-color: #fff;
+    }
+    &:hover {
+      border-color: #fff;
+    }
   }
 
   input {
-    color: white;
-    font-size: 15px;
+    color: #fff;
+    height: 30px;
+    padding-bottom: 2px;
+    border-color: #fff;
   }
 
   label {
@@ -34,10 +45,22 @@ const StyledTextField = styled(MuiTextField)`
       color: #fff;
     }
   }
+
+  & .MuiOutlinedInput-root {
+    & fieldset {
+      border-color: #fff;
+    }
+    &:hover fieldset {
+      border-color: #fff;
+    }
+    &.Mui-focused fieldset {
+      border-color: #fff;
+    }
+  }
 `;
 
 const TextField: React.FC<TextFieldProps> = (props: TextFieldProps) => {
-  return <StyledTextField fullWidth {...props} />;
+  return <StyledTextField variant="standard" fullWidth {...props} />;
 };
 
 export default TextField;
