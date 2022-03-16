61f438a82fd9eb4daeb0e10d3a8b9626fd69e9c2
Ahmed <ahmnouira@gmail.com>
2022-03-16 18:39:06 +0100

    fix textfield black color

--- components/CTA/index.tsx
+++ components/CTA/index.tsx
@@ -1,10 +1,22 @@
 import { Grid } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 import Toast from 'components/Toast';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { FormEvent, useEffect, useState } from 'react';
 import TextField from './components/TextField';
 import styles from './CTA.module.scss';
+
+const theme = createTheme({
+  palette: {
+    secondary: {
+      main: '#fff',
+      light: '#fff',
+      dark: '#fff',
+    },
+  },
+});
 
 const CTA = () => {
   const router = useRouter();
@@ -43,85 +55,90 @@ const CTA = () => {
           <h4>Connect With Us</h4>
           <p>Speak to a team member to learn more about the Pillar commercial real estate marketplace.</p>
         </div>
-        <div className={styles['cta-content-part']}>
-          <form method="POST" onSubmit={handleSubmit}>
-            <input type="hidden" name="oid" value="00D5f000006OVNu" />
-            <input type="hidden" name="retURL" value={`${baseUrl}?success=true&cta=true`} />​
-            {/* <!--  ----------------------------------------------------------------------  -->
+        <ThemeProvider theme={theme}>
+          <div className={styles['cta-content-part']}>
+            <form method="POST" onSubmit={handleSubmit}>
+              <input type="hidden" name="oid" value="00D5f000006OVNu" />
+              <input type="hidden" name="retURL" value={`${baseUrl}?success=true&cta=true`} />​
+              {/* <!--  ----------------------------------------------------------------------  -->
             <!--  NOTE: These fields are optional debugging elements. Please uncomment    -->
             <!--  these lines if you wish to test in debug mode.                          -->
             <input type="hidden" name="debug" value={1} />
             <input type="hidden" name="debugEmail" value="steve@pillarmarkets.com" />
             <!--  ----------------------------------------------------------------------  --> */}
-            {/* the code above is for testing */}
-            <Grid container spacing={2} columnSpacing={4}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  id="first_name"
-                  label="First Name"
-                  inputProps={{ maxLength: 80 }}
-                  name="first_name"
-                  type="text"
-                  placeholder="First Name"
-                />
+              {/* the code above is for testing */}
+              <Grid container spacing={2} columnSpacing={4}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="first_name"
+                    label="First Name"
+                    inputProps={{ maxLength: 80 }}
+                    name="first_name"
+                    type="text"
+                    placeholder="First Name"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="last_name"
+                    label="Last Name"
+                    inputProps={{ maxLength: 80, color: 'white' }}
+                    name="last_name"
+                    color="secondary"
+                    type="text"
+                    placeholder="Last Name"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="company"
+                    label="Company"
+                    inputProps={{ maxLength: 40 }}
+                    name="company"
+                    type="text"
+                    placeholder="Company"
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    id="email"
+                    label="Email"
+                    inputProps={{ maxLength: 80 }}
+                    name="email"
+                    type="email"
+                    placeholder="Email"
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    color="secondary"
+                    style={{ height: 'auto', color: '#fff' }}
+                    id="description"
+                    label="Description"
+                    inputProps={{
+                      color: '#fff',
+                    }}
+                    multiline
+                    rows={3}
+                    InputProps={{
+                      required: false,
+                      disableUnderline: true,
+                    }}
+                    name="description"
+                    type="text"
+                    placeholder={'Description'.toUpperCase()}
+                  />
+                </Grid>
               </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  id="last_name"
-                  label="Last Name"
-                  inputProps={{ maxLength: 80 }}
-                  name="last_name"
-                  type="text"
-                  placeholder="Last Name"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  id="company"
-                  label="Company"
-                  inputProps={{ maxLength: 40 }}
-                  name="company"
-                  type="text"
-                  placeholder="Company"
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  id="email"
-                  label="Email"
-                  inputProps={{ maxLength: 80 }}
-                  name="email"
-                  type="email"
-                  placeholder="Email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  style={{ height: 'auto', color: '#fff' }}
-                  id="description"
-                  label="Description"
-                  inputProps={{
-                    color: '#fff',
-                  }}
-                  multiline
-                  rows={3}
-                  InputProps={{
-                    required: false,
-                  }}
-                  name="description"
-                  type="text"
-                  placeholder={'Description'.toUpperCase()}
-                />
-              </Grid>
-            </Grid>
-            <p className={styles['small']}>
-              By submitting your information, you agree to the{' '}
-              <Link href="/privacy-and-cookies">privacy policy</Link> and to learn more about offers and
-              promotions from Pillar.
-            </p>
-            <input className="ui-button secondary" type="submit" name="submit" value={'Submit'} />
-          </form>
-        </div>
+              <p className={styles['small']}>
+                By submitting your information, you agree to the{' '}
+                <Link href="/privacy-and-cookies">privacy policy</Link> and to learn more about offers and
+                promotions from Pillar.
+              </p>
+              <input className="ui-button secondary" type="submit" name="submit" value={'Submit'} />
+            </form>
+          </div>
+        </ThemeProvider>
       </div>
       <Toast
         open={success}
